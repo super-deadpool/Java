bdd958e3305b2eefb33541a677d5e4959819df31
wade wilson <mystic.c137@gmail.com>
2026-09-06 13:11:29 +0530

    feat#configure : github actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://super-deadpool.github.io',
   markdown: {
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark-dimmed' },
