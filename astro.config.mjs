d31ab5e40ea177a305b13b68af1a73556f1913d6
wade wilson <mystic.c137@gmail.com>
2026-09-06 14:06:16 +0530

    fix github pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://super-deadpool.github.io',
+  base: '/Java',
   markdown: {
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark-dimmed' },
