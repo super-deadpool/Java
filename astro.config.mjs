0603928f1185d283419cc0e05be35962f32622f7
wade wilson <mystic.c137@gmail.com>
2026-09-06 14:37:51 +0530

    feat#deployment : some more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const env = loadEnv('production', process.cwd(), '');
+
 
 export default defineConfig({
   site: 'https://super-deadpool.github.io',
-  base: import.meta.env.PUBLIC_BASE_URL,
+  base: env.PUBLIC_BASE_URL || '/',
   markdown: {
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark-dimmed' },
